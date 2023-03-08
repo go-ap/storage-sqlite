ea0f34cbba790137578c5f4bd094de57ab193283
Marius Orcsik <marius@federated.id>
2023-03-08 10:50:04 +0100

    Adding save key method to storage

--- repository.go
+++ repository.go
@@ -2,6 +2,10 @@ package sqlite
 
 import (
 	"crypto"
+	"crypto/dsa"
+	"crypto/ecdsa"
+	"crypto/ed25519"
+	"crypto/rsa"
 	"crypto/x509"
 	"database/sql"
 	"encoding/pem"
@@ -886,4 +890,79 @@ func flattenCollections(it vocab.Item) error {
 		o.Shares = vocab.FlattenToIRI(o.Shares)
 		return nil
 	})
+}
+
+// SaveKey saves a private key for an actor found by its IRI
+func (r *repo) SaveKey(iri vocab.IRI, key crypto.PrivateKey) (vocab.Item, error) {
+	f, _ := filters.FiltersFromIRI(iri)
+	result, err := loadFromActors(r, f)
+	if err != nil {
+		return nil, err
+	}
+	if result.Count() == 0 {
+		return nil, errors.NotFoundf("not found %s", iri)
+	}
+
+	ob := result.First()
+	typ := ob.GetType()
+	if !vocab.ActorTypes.Contains(typ) {
+		return ob, errors.Newf("trying to generate keys for invalid ActivityPub object type: %s", typ)
+	}
+	actor, err := vocab.ToActor(ob)
+	if err != nil {
+		return ob, errors.Newf("trying to generate keys for invalid ActivityPub object type: %s", typ)
+	}
+
+	m, err := r.LoadMetadata(iri)
+	if err != nil && !errors.IsNotFound(err) {
+		return ob, err
+	}
+	if m.PrivateKey != nil {
+		r.logFn("actor %s already has a private key", iri)
+	}
+	prvEnc, err := x509.MarshalPKCS8PrivateKey(key)
+	if err != nil {
+		r.errFn("unable to x509.MarshalPKCS8PrivateKey() the private key %T for %s", key, iri)
+		return ob, err
+	}
+
+	m.PrivateKey = pem.EncodeToMemory(&pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: prvEnc,
+	})
+	if err = r.SaveMetadata(*m, iri); err != nil {
+		r.errFn("unable to save the private key %T for %s", key, iri)
+		return ob, err
+	}
+
+	var pub crypto.PublicKey
+	switch prv := key.(type) {
+	case *ecdsa.PrivateKey:
+		pub = prv.Public()
+	case *rsa.PrivateKey:
+		pub = prv.Public()
+	case *dsa.PrivateKey:
+		pub = &prv.PublicKey
+	case *ed25519.PrivateKey:
+		pub = prv.Public()
+	default:
+		r.errFn("received key %T does not match any of the known private key types", key)
+		return ob, nil
+	}
+	pubEnc, err := x509.MarshalPKIXPublicKey(pub)
+	if err != nil {
+		r.errFn("unable to x509.MarshalPKIXPublicKey() the private key %T for %s", pub, iri)
+		return ob, err
+	}
+	pubEncoded := pem.EncodeToMemory(&pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: pubEnc,
+	})
+
+	actor.PublicKey = vocab.PublicKey{
+		ID:           vocab.IRI(fmt.Sprintf("%s#main", iri)),
+		Owner:        iri,
+		PublicKeyPem: string(pubEncoded),
+	}
+	return r.Save(actor)
 }
