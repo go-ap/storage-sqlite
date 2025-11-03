7ee2cb1ec36915f6ad4a4fdd483ce58936696292
Marius Orcsik <marius@federated.id>
2025-11-03 14:54:14 +0100

    Adding connection settings meant to minimize contention on writes
    We have separate query parameters specific to each of the two drivers we use

--- no_cgo.go
+++ no_cgo.go
@@ -4,12 +4,28 @@ package sqlite
 
 import (
 	"database/sql"
+	"net/url"
 
 	_ "modernc.org/sqlite"
 )
 
+var defaultQueryParam = url.Values{
+	"_txlock": []string{"immediate"},
+	"_pragma": []string{
+		// Faster synchronization that still keeps the data safe:
+		//"temp_store=MEMORY",
+		//"synchronous=NORMAL",
+		// Increase cache size (in this case to 64MB), the default is 2MB
+		//"cache_size=-64000",
+		// from BJohnson's recommendations to use with litestream
+		//"journal_mode=WAL",
+		"busy_timeout=5000",
+		//"wal_autocheckpoint=0",
+	},
+}
+
 // sqlOpen will use the learnc.org/sqlite when compiled without CGO
 // this driver is less performant.
 var sqlOpen = func(dataSourceName string) (*sql.DB, error) {
-	return sql.Open("sqlite", dataSourceName)
+	return sql.Open("sqlite", dataSourceName+"?"+defaultQueryParam.Encode())
 }
--- with_cgo.go
+++ with_cgo.go
@@ -4,12 +4,20 @@ package sqlite
 
 import (
 	"database/sql"
+	"net/url"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var defaultQueryParam = url.Values{
+	"_txlock": []string{"immediate"},
+	// from BJohnson's recommendations to use with litestream
+	//"_journal_mode": []string{"WAL"},
+	"_busy_timeout": []string{"5000"},
+}
+
 // sqlOpen will use the github.com/mattn/go-sqlite3 package when compiled with CGO
 // this driver is more performant but, as said, it requires CGO
 var sqlOpen = func(dataSourceName string) (*sql.DB, error) {
-	return sql.Open("sqlite3", dataSourceName)
+	return sql.Open("sqlite3", dataSourceName+"?"+defaultQueryParam.Encode())
 }
